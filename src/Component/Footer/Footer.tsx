01a44cc2b5a3141441611c3bb8bc56446e726778
Dawood Ahmad <dawood11@hotmail.com>
2020-01-19 21:50:01 +0100

    added footer

---
+++ src/Component/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+import React from 'react';
+
+const Footer: React.FC = () => {
+	return (
+		<div className={'footer'}>
+			Dagbladet
+		</div>
+	);
+};
+
+export default Footer;
